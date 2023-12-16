2a37a94d9491dabbcfa5aff54926c4964641bfd0
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-12-16 21:16:06 +0545

    Addtion of get hhtp request in app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
+/*
 app.get('/', (req, res) => {
   res.status(404).json({
     message: 'Hallo from the server side!!',
@@ -11,6 +13,21 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('You can PoST in this URL...');
+});
+*/
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
