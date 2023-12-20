ab494ff53510dabd7c62a16481d650048de28acb
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-12-20 19:21:13 +0545

    Adding new item through POST

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 
+// This is the middleware
 app.use(express.json());
 
 /*
@@ -33,8 +34,23 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
