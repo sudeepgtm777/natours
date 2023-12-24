d05d04a4bf97e60e3fb581015dfab92628165699
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-12-24 17:02:27 +0545

    Responding to url parameters

--- app.js
+++ app.js
@@ -33,6 +33,20 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
