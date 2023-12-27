5a0582a5da422107c8584c1d3200bd22c4111df9
sudeepgtm777 <sudeepgautam777@gmail.com>
2023-12-27 19:34:20 +0545

    Handling delete requests

--- app.js
+++ app.js
@@ -92,6 +92,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'Fail',
+      message: 'Invalid id',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running in ${port} port!!`);
