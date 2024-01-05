95b01ec1844c7518d11dc50ffb151d6bda73eaa7
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-01-05 20:08:35 +0545

    Update

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
-
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running in ${port} port!!`);
 });
