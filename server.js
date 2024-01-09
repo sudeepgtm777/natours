a99b94cb5eda8cc9de7c8d8d4440fde8af506a27
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-01-09 22:09:26 +0545

    Refactoring for MVC(Model View Controller)

--- server.js
+++ server.js
@@ -18,39 +18,6 @@ mongoose
   })
   .then(() => console.log('Suceesful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a Name!!'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a Price!!'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-// This is the instance of Tour model
-const testTour = new Tour({
-  name: 'The House Of The Dragon',
-  rating: 9.5,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('Error!!', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running in ${port} port!!`);
