eb622fd311c5dded5946ca458be6e2ff814a1ea1
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-01-15 20:54:40 +0545

    Adding router for tour-stats

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+
 const checkBody = router
   .route('/')
   .get(tourController.getAllTours)
