13b1228e9e9b1c6e24c11210b74028a6ca937c0a
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-02-10 21:28:30 +0545

    Implementing different userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -1,2 +1,28 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A user must have a name!!!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A use must have Email!!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Use a valid Email!!1'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Provide a password!1'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Confirm the password'],
+  },
+});
