d60b42918d424dada4163a8609ae5c77329b8fed
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-02-13 20:11:23 +0545

    Adding route for authcontroller.protect

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
@@ -13,9 +14,9 @@ router
 router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-const checkBody = router
+router
   .route('/')
-  .get(tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
