21dc27a74c01faefd689fb61d0f8d6c06ca559fa
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-05 22:34:09 +0545

    Testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -120,7 +120,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   //  2)  Generate the random reset token
 
   const resetToken = user.createPasswordResetToken();
-  await user.save();
+  await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
 });
