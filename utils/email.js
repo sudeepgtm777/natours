573c0dd19c6e3253a6beb93deb289a8f44e5bfff
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-06 20:16:14 +0545

    Creating a transporter for mailtrap

--- utils/email.js
+++ utils/email.js
@@ -3,12 +3,12 @@ const nodemailer = require('nodemailer');
 const sendEmail = (options) => {
   // 1) Create a transportor
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
-    /***  Activate in gmail "less secure app" option  ***/
   });
 
   // 2) Define the email options
