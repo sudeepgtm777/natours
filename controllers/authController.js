f1a938014442db3ab76003cec5e1af4b65f3fd97
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-14 17:30:17 +0545

    Removing the password while sigup is done

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,6 +26,9 @@ const createAndSendToken = (user, statusCode, res) => {
 
   res.cookie('jwt', token, cookieOptions);
 
+  // Remove the password from the output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
