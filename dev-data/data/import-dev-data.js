bc346931e153687c79317796f03341133dced295
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-16 22:06:02 +0545

    Importing new data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,9 +20,7 @@ mongoose
   .then(() => console.log('Suceesful'));
 
 //   Read JSON File
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // Import Data into Database
 const importData = async () => {
