0b5691e4054f025bc45bf1ec83947bb67736e703
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-18 18:59:10 +0545

    Creating review Schema

---
+++ models/reviewModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new moongose.Schema({
+  review: {
+    type: String,
+    required: [true, 'There must be a review!!!'],
+  },
+
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
