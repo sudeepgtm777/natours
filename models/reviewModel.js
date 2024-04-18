3c4f9613424ee6b78cfd54122b68ea9619913e87
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-18 21:33:52 +0545

    Adding virtual property toJson and toObjects

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,34 +1,40 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new moongose.Schema({
-  review: {
-    type: String,
-    required: [true, 'There must be a review!!!'],
-  },
+const reviewSchema = new moongose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'There must be a review!!!'],
+    },
 
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
 
-  tour: {
-    type: moongose.Schema.ObjectId,
-    ref: 'Tour',
-    required: [true, 'Review must be associated with tour'],
-  },
+    tour: {
+      type: moongose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must be associated with tour'],
+    },
 
-  user: {
-    type: moongose.Schema.ObjectId,
-    ref: 'User',
-    required: [true, 'Review must be associated with user'],
+    user: {
+      type: moongose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must be associated with user'],
+    },
   },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
 
 const Review = moongose.model('Review', reviewSchema);
 
