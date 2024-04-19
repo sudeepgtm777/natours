3e6a1ce46791f96352abdf01304bc36f77fc5faa
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-19 16:06:34 +0545

    Adding route for getAllReviews and createReview

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const reviewController = require('./../controllers/reviewController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(reviewController.createReview);
+
+module.exports = router;
