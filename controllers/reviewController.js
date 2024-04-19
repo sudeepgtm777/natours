756bdab092a1d33ddcf7646a0feec8841a33fd3f
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-19 14:17:41 +0545

    Exporting all the reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,3 +1,15 @@
 const Review = require('./../models/reviewModel');
-const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  // Send Response to the user
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
