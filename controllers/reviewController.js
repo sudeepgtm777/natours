74475ba782c1524d680023f639095c2548d420f5
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-24 20:11:19 +0545

    Exporting setTourUserIds

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -22,6 +22,7 @@ exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
 };
 
 /****** The use of factory model to create Review. *******/
