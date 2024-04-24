4a9f22f8c9421269800e0d8b1baacacb5a97eb79
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-24 14:36:51 +0545

    Debugging

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,7 @@ const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const factory = require('./handleFactory  ');
+const factory = require('./handleFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
