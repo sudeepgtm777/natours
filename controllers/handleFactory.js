816013f7ee55b703f9714c69bbafe66c50a1acd3
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-24 14:27:17 +0545

    Importing files

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,3 +1,6 @@
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
