c799ec093945c672c0808a4a11fe43408478f200
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-25 19:43:01 +0545

    Implementing getAll for all other controllers

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -59,6 +60,26 @@ exports.getOne = (Model, popOptions) =>
 
     res.status(200).json({
       status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    //Execute The Query
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    //Send Response
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
       data: {
         data: doc,
       },
