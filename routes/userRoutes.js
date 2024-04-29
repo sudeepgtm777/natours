2b0d51b17d35ac9b0343878e949c83e320ba85a5
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-29 21:59:10 +0545

    Adding route of authController.protect in router to use it in every field after the call

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,20 +10,14 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updatePassword',
-  authController.protect,
-  authController.updatePassword,
-);
+// This runs after above middlewares and use of authController.protect is done in every middleware.
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updatePassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router.route('/').get(userController.getAllUsers);
 
