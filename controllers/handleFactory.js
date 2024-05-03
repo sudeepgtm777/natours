49906656c067a64a8a53b2415d9e8da18539a1da
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-05-03 09:57:51 +0545

    Adding explain in getAll

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -78,7 +78,7 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query;
+    const doc = await features.query.explain();
 
     //Send Response
     res.status(200).json({
