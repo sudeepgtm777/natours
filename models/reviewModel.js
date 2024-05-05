418d88b4599d115f9147a409b0ea2aacd52420c5
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-05-05 10:14:45 +0545

    Combination of tour and review for unique value.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,6 +37,8 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   /******* This is used for getting data of both tour and user from id ********/
   /*
