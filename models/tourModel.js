5d84150225456de8df7e58d3484cf57fa352783b
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-05-07 21:46:50 +0545

    Commenting the Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -186,10 +186,12 @@ tourSchema.post(/^find/, function (doc, next) {
 });
 
 // Aggregation Middleware
+/*
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
+*/
 
 const Tour = mongoose.model('Tour', tourSchema);
 
