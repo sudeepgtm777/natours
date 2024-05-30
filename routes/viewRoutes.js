4ba210fbdfc36d82eab8246e3c66b7ba2e85392e
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-05-30 21:09:52 +0545

    Adding routes and status for getOverview and getTour

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,24 +1,9 @@
 const express = require('express');
+const viewsController = require('./../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Sudeep',
-  });
-});
-
-router.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-router.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
-  });
-});
+router.get('/', viewsController.getOverview);
+router.get('/tour', viewsController.getTour);
 
 module.exports = router;
