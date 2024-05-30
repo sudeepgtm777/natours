8290fae0d53df83817659a2585e2bd7c4cddcae8
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-05-30 20:55:24 +0545

    Refactoring code from app.js to viewRoutes.js

--- app.js
+++ app.js
@@ -68,24 +68,6 @@ app.use((req, res, next) => {
 });
 
 // Route
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Sudeep',
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
-  });
-});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Sudeep',
+  });
+});
+
+router.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+router.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
+
 module.exports = router;
