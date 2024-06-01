1aab5e5dc1135ce50d92094869787365fb9ba422
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-01 20:22:46 +0545

    Debugging

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -23,7 +23,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 
   res.status(200).render('tour', {
-    title: tour.name,
+    title: `${tour.name} Tour`,
     tour,
   });
   next();
