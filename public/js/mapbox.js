41b6a5f6c999fd8a4c02764284065e742c5cd339
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-04 20:52:55 +0545

    Refactoring code for mapbox and adding some functionalit

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -5,6 +5,43 @@ maptilersdk.config.apiKey = 'K6Cf8hAX1M7bPyicFtab';
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element to render the map
   style: 'basic-v2',
-  center: [16.62662018, 49.2125578], // starting position [lng, lat]
-  zoom: 14, // starting zoom
+  scrollZoom: false,
+  //   center: [-118.113491, 34.111745], // starting position [lng, lat]
+  //   zoom: 4, // starting zoom
+});
+
+const bounds = new maptilersdk.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new maptilersdk.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new maptilersdk.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
