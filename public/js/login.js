e565559b08caf90f46070959e10970264146113f
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-06 15:39:54 +0545

    Use of try catch block for error handling

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,6 @@
-const login = async (email, password) => {
+import axios from 'axios';
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -19,10 +21,3 @@ const login = async (email, password) => {
     alert(err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
