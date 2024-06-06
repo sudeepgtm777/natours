5760a067345628a3e00bdf20559ffa578535f4ce
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-06 17:47:19 +0545

    Use of values inside condition after checking if loginForm exists

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,24 @@
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
 import { login } from './login';
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+// DOM elements
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+
+// Delegation
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //  Values
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
