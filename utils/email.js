4bc5b668e20b7c3f16381c02eeb443d6611031e5
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-16 21:28:58 +0545

    Use of html-to-text to convert html to text

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const htmlToText = require('html-to-text');
+const { convert } = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
@@ -43,7 +43,9 @@ module.exports = class Email {
       to: this.to,
       subject,
       html,
-      text: htmlToText.fromString(html),
+      text: convert(html, {
+        wordwrap: 230,
+      }),
       // html: conversion is done of the text
     };
 
