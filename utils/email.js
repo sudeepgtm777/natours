b45fac1b84a91672aa8295decfb5166d356ebfbc
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-16 18:23:45 +0545

    Implementing email i.e creating, rendering, defining, transporting

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,59 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) Create a transportor
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Sudeep <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: '<sudeep@123.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html: conversion is done of the text
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  // 3) Actually send the email
-  await transporter.sendMail(mailOptions);
+  // Sends the actual email
+  async send(template, subject) {
+    // 1) Render the HTML based on a pug template.
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+      // html: conversion is done of the text
+    };
+
+    // 3) Create a transport and send Email.
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  // The use of this function is to call send() which has wider uses.
+  async sendWelcome() {
+    await this.send('Welcome', 'Welcome to the natours!!');
+  }
 };
-module.exports = sendEmail;
