1d584f9b0d00ca518bd4a8ecf12c494e08dea45a
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-18 21:03:21 +0545

    Implementing config files of brevo

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,14 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        host: process.env.BREVO_HOST,
+        port: process.env.BREVO_PORT,
+        auth: {
+          user: process.env.BREVO_USER,
+          pass: process.env.BREVO_PASSWORD,
+        },
+      });
     }
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
