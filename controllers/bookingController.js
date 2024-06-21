5663d5d53dd732c5fc8030ff563b1570ea957409
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-21 17:05:03 +0545

    Implementing booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,31 +6,38 @@ const factory = require('./handleFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour.
-  const tour = await Tour.findById(req.params.tourID);
+  const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create the checkout session.
-  const session = await stripe.checkout.session.create({
+  const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          'https://raw.githubusercontent.com/sudeepgtm777/natours/main/public/img/logo-green-round.png',
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
+        price_data: {
+          currency: 'USD',
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              'https://raw.githubusercontent.com/sudeepgtm777/natours/main/public/img/logo-green-round.png',
+            ],
+          },
+          unit_amount: tour.price * 100,
+        },
         quantity: 1,
       },
     ],
   });
 
-  // 3) Create sessions as response
-  res.status('200').json({
+  // 3) Create session as response
+  res.status(200).json({
     status: 'success',
     session,
   });
