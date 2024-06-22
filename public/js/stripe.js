d30a08349df4719369006415b159348662e7376d
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-22 18:33:22 +0545

    Use of session to get the tour Id and Implementing checkout of session

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51PTNSiJ0wkAZn6PJhCNyS5oVVOdyoGMfZCBJ5qPpi1mx06qlozG3A0uf2QuusuoTsdpnmEliRJ0IgIiEpTPMNX6B00sqfQ6i5H',
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get Checkout Session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+    console.log(session);
+
+    // 2) Create checkout form + charge for credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
