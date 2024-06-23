4f51206f89782acb5ec2a366b62b4a184d3261bd
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-23 18:51:21 +0545

    Implementing routes for booking

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -4,10 +4,22 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession,
-);
+// The use of authController.protect in this manner makes sure that the authController.protect is used in every route.
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
