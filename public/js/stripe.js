114768ee5f5cb5a6e49916ceba278952ebacea08
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-23 19:38:04 +0545

    Debugging the log

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -10,7 +10,6 @@ export const bookTour = async (tourId) => {
     const session = await axios(
       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
     );
-    console.log(session);
 
     // 2) Create checkout form + charge for credit card
     await stripe.redirectToCheckout({
