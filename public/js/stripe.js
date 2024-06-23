0eb78fd9b3f600b784c764b63c7b74bb2ede9c89
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-23 20:22:51 +0545

    Debugging for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,9 +7,7 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get Checkout Session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2) Create checkout form + charge for credit card
     await stripe.redirectToCheckout({
