01d6f581d6fc2564e001fa036c5c505e06a17180
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-06-25 18:43:02 +0545

    Debugging the signup Page

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,11 +39,17 @@ const createAndSendToken = (user, statusCode, res) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
   const url = `${req.protocol}://${req.get('host')}/me`;
   await new Email(newUser, url).sendWelcome();
 
-  createAndSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
